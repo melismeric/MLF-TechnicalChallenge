39601db5082439ba4306245660145ceca49dac84
melis <zmelismeric@gmail.com>
2023-02-28 14:45:18 +0000

    added test cases

--- hello_test.cc
+++ hello_test.cc
@@ -9,7 +9,7 @@
 #include "clientClass.h"
 
 #define PORT 8080
-#define MAX_CLIENTS 36
+#define MAX_CLIENTS 25
 
 using namespace std::this_thread; // sleep_for, sleep_until
 using namespace std::chrono; // nanoseconds, system_clock, seconds
@@ -28,7 +28,6 @@ Client* CreateClientIfNotExist(vector<Client*>& objects, int id, string address,
     Client* c = GetClient(objects, id);
                 
     if (c != nullptr) {
-        //c->deviceTrigger();
         return c;
     }
     else {
@@ -47,47 +46,6 @@ Client* CreateClientIfNotExist(vector<Client*>& objects, int id, string address,
     }
 }
 
-
-TEST(ClientTest, ClientConnection) {  
-  int id = 1;
-  string address = "127.0.0.1";
-  int port = 8080; 
-
-  Client client(id, address, port);
-  ASSERT_TRUE(client.connectToServer());
-  client.disconnect();
-}
-
-TEST(ClientTest2, ClientStateOnboarding) {  
-  int id = 2;
-  string address = "127.0.0.1";
-  int port = 8080; 
-
-  Client client(id, address, port);
-  client.connectToServer();
-  
-  // State should be ongoing
-    EXPECT_EQ(client.getState(),1);
-    client.disconnect();
-}
-
-TEST(ClientTest3, changeStateRequest) {  
-  int id = 3;
-  string address = "127.0.0.1";
-  int port = 8080; 
-
-  Client client(id, address, port);
-  client.connectToServer();
-  std::chrono::seconds(20);
-  client.changeStateRequest(false);
-  
-  // State should be engaging
-  EXPECT_EQ(client.getState(),1);
-  client.disconnect();
-}
-
-
-
 TEST(LimitTest, limit){
     //clientList
     vector<Client*> clients;
@@ -96,39 +54,26 @@ TEST(LimitTest, limit){
     for(int i = 0; i < MAX_CLIENTS; i++){
         Client* c = CreateClientIfNotExist(clients, i, address, port);
     }
-    Client *lastClient = GetClient(clients, 35);
+    Client *lastClient = GetClient(clients, clients.size()-1);
     // expected state is idle for the last device
     EXPECT_EQ(lastClient->getState(),0);
 }
 
-/*
-// State Request w/ error
-TEST(ClientTest4, changeStateRequestError) {  
-  int id = 4;
+TEST(ClientTest, ClientConnection) {  
+  int id = 1;
+  string address = "127.0.0.1";
+  int port = 8080; 
+
+  Client client(id, address, port);
+  ASSERT_TRUE(client.connectToServer());
+}
+
+TEST(ClientTest, ClientOnboarding) {  
+  int id = 1;
   string address = "127.0.0.1";
   int port = 8080; 
 
   Client client(id, address, port);
   client.connectToServer();
-  std::chrono::seconds(20);
-  client.changeStateRequest(true);
-  std::chrono::seconds(100);
-  // State should be engaging
-  EXPECT_EQ(client.getState(),4);
-  client.disconnect();
-} 
-
-TEST(ClientTest5, disconnect) { 
-  vector<Client*> clients; 
-  int id = 4;
-  string address = "127.0.0.1";
-  int port = 8080; 
-
-  Client* c = CreateClientIfNotExist(clients, id, address, port);
-   c->changeStateRequest(true);
-  
-  // State should be idle
-  //c->disconnect();
-  //std::chrono::seconds(100);
-  EXPECT_EQ(c->getState(),0);
-}*/
+  EXPECT_EQ(client.getState(),1);
+}
